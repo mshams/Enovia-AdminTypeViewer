46bfeca1165733a4222d6848bf90c13185115982
DC\Shams <mshamsj@gmail.com>
2021-01-19 11:30:47 +0330

    Adding classes templates

--- src/jpo/AdminTypeTable_mxJPO.java
+++ src/jpo/AdminTypeTable_mxJPO.java
@@ -1,4 +1,15 @@
 package jpo;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
 public class AdminTypeTable_mxJPO {
+
+    @com.matrixone.apps.framework.ui.ProgramCallable
+    public Vector getParamByColumnName(Context context, String[] args) throws Exception {
+        Vector vResult = new Vector();
+
+        return vResult;
+    }
 }
--- src/jpo/AdminTypesHelper_mxJPO.java
+++ src/jpo/AdminTypesHelper_mxJPO.java
@@ -1,4 +1,51 @@
 package jpo;
 
 public class AdminTypesHelper_mxJPO {
+    @com.matrixone.apps.framework.ui.ProgramCallable
+    public MapList getTypesList(Context context, String[] args) throws Exception {
+        MapList result = new MapList();
+
+        try {
+
+            return result;
+        } catch (Exception e) {
+            throw new Exception("getTypesList", e);
+        }
+    }
+
+    @com.matrixone.apps.framework.ui.ProgramCallable
+    public MapList getPoliciesList(Context context, String[] args) throws Exception {
+        MapList result = new MapList();
+
+        try {
+
+            return result;
+        } catch (Exception e) {
+            throw new Exception("getPoliciesList", e);
+        }
+    }
+
+    @com.matrixone.apps.framework.ui.ProgramCallable
+    public MapList getRelsList(Context context, String[] args) throws Exception {
+        MapList result = new MapList();
+
+        try {
+
+            return result;
+        } catch (Exception e) {
+            throw new Exception("getRelsList", e);
+        }
+    }
+
+    @com.matrixone.apps.framework.ui.ProgramCallable
+    public MapList getCommandsList(Context context, String[] args) throws Exception {
+        MapList result = new MapList();
+
+        try {
+
+            return result;
+        } catch (Exception e) {
+            throw new Exception("getCommandsList", e);
+        }
+    }
 }
