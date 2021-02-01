1b6a3a0889f9afc2549d1f5b09911e36bc757271
DC\Shams <mshamsj@gmail.com>
2021-02-01 11:59:02 +0330

    Create dto class

--- src/jpo/dto/AdminType_mxJPO.java
+++ src/jpo/dto/AdminType_mxJPO.java
@@ -6,7 +6,12 @@
 
 package jpo.dto;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
 public class AdminType_mxJPO {
+    private static final String ATT_SEPARATOR = "|";
     private String name;
     private String description;
     private String originated;
@@ -24,6 +29,35 @@ public class AdminType_mxJPO {
     public AdminType_mxJPO() {
     }
 
+    public static AdminType_mxJPO createFromString(String buffer, AdminTypeName typeName) {
+        AdminType_mxJPO obj = null;
+        try {
+            String[] values = buffer.split(ATT_SEPARATOR);
+            if (values.length == 4) {
+                obj = new AdminType_mxJPO(
+                        values[0],
+                        values[1],
+                        values[2],
+                        values[3],
+                        typeName
+                );
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            obj = null;
+        }
+
+        return obj;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", originated='" + originated + '\'' +
+                ", modified='" + modified + '\'' +
+                ", type=" + type.name();
+    }
 }
 
 enum AdminTypeName {
