4d184ceb756989a44d62ab8f946509dcbf951da5
DC\Shams <mshamsj@gmail.com>
2021-03-01 14:50:58 +0330

    Complete method getParamByColumnName

--- src/jpo/AdminTypeTable_mxJPO.java
+++ src/jpo/AdminTypeTable_mxJPO.java
@@ -6,7 +6,9 @@
 
 package jpo;
 
+import com.matrixone.apps.domain.util.MapList;
 import matrix.db.Context;
+import matrix.db.JPO;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +19,23 @@ public class AdminTypeTable_mxJPO {
     @com.matrixone.apps.framework.ui.ProgramCallable
     public Vector getParamByColumnName(Context context, String[] args) throws Exception {
         Vector vResult = new Vector();
+        HashMap programMap = JPO.unpackArgs(args);
+        HashMap mapColumn = (HashMap) programMap.get("columnMap");
+        String paramName = (String) mapColumn.get("name");
+        MapList mapListObjects = (MapList) programMap.get("objectList");
 
+        for (Object mapListObject : mapListObjects) {
+            Map mapItem = (Map) mapListObject;
+            String result;
+
+            try {
+                result = mapItem.get(paramName).toString();
+            } catch (Exception e) {
+                result = "-";
+            }
+
+            vResult.add(result);
+        }
         return vResult;
     }
 }
