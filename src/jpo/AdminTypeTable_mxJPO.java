c357b06b910149e74476058cd5473dbca72e43be
DC\Shams <mshamsj@gmail.com>
2021-03-10 17:28:53 +0330

    Set HTML link

--- src/jpo/AdminTypeTable_mxJPO.java
+++ src/jpo/AdminTypeTable_mxJPO.java
@@ -6,15 +6,24 @@
 
 package jpo;
 
+import com.matrixone.apps.domain.util.FrameworkException;
 import com.matrixone.apps.domain.util.MapList;
+import com.matrixone.apps.domain.util.MqlUtil;
+import com.matrixone.apps.framework.ui.UIUtil;
 import matrix.db.Context;
 import matrix.db.JPO;
+import matrix.util.StringList;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Vector;
 
 public class AdminTypeTable_mxJPO {
+    private static String LINK_SHOW_TYPE = "emxPLMOnlineAdminTypeReport.jsp?typeName=%s";
+    private static String LINK_SHOW_REL = "emxPLMOnlineAdminRelationshipReport.jsp?relationshipName=%s";
+    private static String LINK_SHOW_CMD = "emxPLMOnlineAdminCommandReport.jsp?CommandName=%s";
+    private static String LINK_SHOW_POLICY = "emxPLMOnlineAdminPolicyReport.jsp?policyName=%s";
 
     @com.matrixone.apps.framework.ui.ProgramCallable
     public Vector getParamByColumnName(Context context, String[] args) throws Exception {
@@ -35,6 +44,56 @@ public class AdminTypeTable_mxJPO {
             }
 
             vResult.add(result);
+        }
+        return vResult;
+    }
+
+    @com.matrixone.apps.framework.ui.ProgramCallable
+    public Vector getHyperlinkColumn(Context context, String[] args) throws FrameworkException {
+        Vector vResult = new Vector();
+
+        try {
+            HashMap programMap = JPO.unpackArgs(args);
+            Map paramListMap = (Map) programMap.get("paramList");
+            MapList mapListObjects = (MapList) programMap.get("objectList");
+
+            String hrefBaseURL = "";
+            String progName = (String) paramListMap.get("program");
+            switch (progName.trim()) {
+                case "jpo.AdminTypesHelper:getTypesList":
+                    hrefBaseURL = LINK_SHOW_TYPE;
+                    break;
+                case "jpo.AdminTypesHelper:getCommandsList":
+                    hrefBaseURL = LINK_SHOW_CMD;
+                    break;
+                case "jpo.AdminTypesHelper:getPoliciesList":
+                    hrefBaseURL = LINK_SHOW_POLICY;
+                    break;
+                case "jpo.AdminTypesHelper:getRelsList":
+                    hrefBaseURL = LINK_SHOW_REL;
+                    break;
+            }
+
+            for (Object mapListObject : mapListObjects) {
+                Map mapItem = (Map) mapListObject;
+                String typeName;
+                String hrefStr;
+
+                try {
+                    typeName = mapItem.get("name").toString();
+                    hrefStr = String.format(hrefBaseURL, typeName);
+                    typeName = String.format(
+                            "<a href='%s' target='_blank'>Open AdminType</a>",
+                            hrefStr
+                    );
+                } catch (Exception e) {
+                    typeName = "-";
+                }
+
+                vResult.add(typeName);
+            }
+        } catch (Exception e) {
+
         }
         return vResult;
     }
